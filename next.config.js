f1e07c789eb0dc1b91687d85b330262440ae902f
jedondfds <jekabs.donis@gmail.com>
2020-10-06 21:53:29 +0200

    make vercel url abs

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-console.log({
-  vercelUrl: process.env.VERCEL_URL,
-});
+const vercelUrl = process.env.VERCEL_URL.includes("http")
+  ? process.env.VERCEL_URL
+  : `https://${process.env.VERCEL_URL}`;
 module.exports = {
   env: {
     API_URL: "https://bm-materials.com",
-    URL: process.env.VERCEL_URL,
+    URL: vercelUrl,
   },
 };
